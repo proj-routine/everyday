b4a8b0080e0752e77ad719b283f6ecf4f436bbfb
ffflopppy <ffflopppy@gmail.com>
2022-09-04 16:42:14 +0900

    first commit

---
+++ everyday-be/src/main/java/com/routine/everydaybe/EverydayBeApplication.java
@@ -0,0 +1,13 @@
+package com.routine.everydaybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverydayBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverydayBeApplication.class, args);
+	}
+
+}
---
+++ everyday-be/src/test/java/com/routine/everydaybe/EverydayBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.routine.everydaybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverydayBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
