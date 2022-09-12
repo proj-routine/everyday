5e781acfb5154f33d2304ebe5b6c437c116fe81a
ffflopppy <ffflopppy@gmail.com>
2022-09-12 23:50:18 +0900

    update : controller

--- everyday-be/src/main/java/com/routine/everydaybe/controller/PersonalRecordsController.java
+++ everyday-be/src/main/java/com/routine/everydaybe/controller/PersonalRecordsController.java
@@ -6,12 +6,18 @@ import com.routine.everydaybe.dto.PersonalRecordsByDateQueryDTO;
 import com.routine.everydaybe.vo.DateByMonthQueryVO;
 import com.routine.everydaybe.vo.PersonalRecordDataByDateQueryVO;
 import com.routine.everydaybe.vo.PersonalRecordsByDateQueryVO;
+import com.routine.everydaybe.vo.TestVO;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-
+@RestController
 @RequestMapping("/everyday/records")
 public class PersonalRecordsController {
-
+    @Operation(summary = "test", description = "test", method = "GET")
+    @GetMapping("/test")
+    public TestVO test() {
+        return TestVO.builder().test("test").build();
+    }
     @GetMapping
     public DateByMonthQueryVO dateByMonthQuery(DateByMonthQueryDTO dto) {
         return null;
