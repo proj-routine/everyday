7d845dcf2408ddbec7b4d2efa84ffc047c42d007
sam <j2d1005@naver.com>
2022-09-18 23:18:00 +0900

    Update 100vh 셋팅, AppLayout minHeight

--- everyday-fe/src/components/layouts/AppLayout.tsx
+++ everyday-fe/src/components/layouts/AppLayout.tsx
@@ -1,16 +1,32 @@
 import { APP_MAX_WIDTH } from '@constants/constants';
 import { Container } from '@mui/material';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 type Props = React.PropsWithChildren;
 
 export default function AppLayout({ children }: Props) {
+  useEffect(() => {
+    function setScreenSize() {
+      let vh = window.innerHeight * 0.01;
+
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+
+    setScreenSize();
+
+    window.addEventListener('resize', setScreenSize);
+
+    return () => {
+      window.removeEventListener('resize', setScreenSize);
+    };
+  }, []);
+
   return (
     <Container
       sx={{
         display: 'flex',
         maxWidth: `${APP_MAX_WIDTH}px !important`,
-        minHeight: '100vh',
+        minHeight: 'var(--100vh)',
         flexDirection: 'column',
         paddingX: '0 !important',
         boxShadow: '0px 0px 16px rgb(50 50 50 / 12%)',
